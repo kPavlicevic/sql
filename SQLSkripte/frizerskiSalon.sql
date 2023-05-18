0f9d740ee98e86f1b5ed0e433483710bcdef9cd1
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-05-18 20:36:16 +0200

    Update frizerskiSalon.sql

--- SQLSkripte/frizerskiSalon.sql
+++ SQLSkripte/frizerskiSalon.sql
@@ -1,7 +1,7 @@
-use master;
+
 drop database if exists frizerskiSalon;
 go
-create database frizerskiSalon;
+create database frizerskiSalon collate Croatian_CI_AS;
 go
 use frizerskiSalon;
 
@@ -27,13 +27,46 @@ create table usluga(
 
 create table korisnik(
 	sifra int not null primary key identity (1,1),
-	spol bit,
+	spol varchar,
 	duzinaKose decimal (5,2)
 );
 
 alter table termin add foreign key (djelatnik) references djelatnik(sifra);
 alter table termin add foreign key (usluga) references usluga(sifra);
 alter table termin add foreign key (korisnik) references korisnik(sifra);
+
+select * from korisnik;
+
+insert into korisnik (spol,duzinaKose)
+values
+	('z','30.15'),
+	('m','10.05'),
+	('z','50.55');
+
+
+select * from djelatnik;
+
+insert into djelatnik (ime,prezime)
+values
+	('Ivanka','Ivaniæ'),
+	('Marija','Mariæ'),
+	('Danijela','Daniæ');
+
+select * from usluga;
+
+insert into usluga (naziv,cijena)
+values
+	('frizura','50'),
+	('sisanje','15'),
+	('farbanje','32.55');
+
+select * from termin;
+
+insert into termin (datum,korisnik,usluga,djelatnik)
+values
+	('2023-06-10',1,1,1),
+	('2023-06-05',2,2,2),
+	('2023-05-25',3,3,3);
 
 
 
