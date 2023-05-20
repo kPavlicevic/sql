40ab7d418e77afe1de3bfcc58c087dacaa9a6b94
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-05-20 14:30:52 +0200

    insert

--- SQLSkripte/muzej.sql
+++ SQLSkripte/muzej.sql
@@ -22,7 +22,7 @@ create table kustos (
 	sifra int not null primary key identity (1,1),
 	ime varchar (50) not null,
 	prezime varchar (50) not null,
-	izlozba int not null
+	izlozba int
 );
 
 create table sponzor (
@@ -35,3 +35,21 @@ create table sponzor (
 alter table djelo add foreign key (izlozba) references izlozba(sifra);
 alter table izlozba add foreign key (sponzor) references sponzor(sifra);
 alter table izlozba add foreign key (kustos) references kustos(sifra);
+
+
+select * from sponzor;
+
+insert into sponzor (ime,prezime,donacija)
+values
+	('Mara','Mariæ','1500'),
+	('Mile','Miliæ','2000'),
+	('Kata','Katiæ','3900');
+
+select * from kustos;
+
+insert into kustos (ime,prezime,izlozba)
+values
+	('Valentina','Viliæ','z'),
+	('Ante','Antiæ','y'),
+	('Hrvoje','Horvat','x');
+
