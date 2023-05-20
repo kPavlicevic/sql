817d33daaf50d5111c8c2353cf1defa98c0b24f0
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-05-20 14:01:40 +0200

    update, delete

--- SQLSkripte/djecjiVrtic.sql
+++ SQLSkripte/djecjiVrtic.sql
@@ -28,3 +28,38 @@ create table odgajateljica (
 
 alter table dijete add foreign key (odgojnaSkupina) references odgojnaSkupina(sifra);
 alter table odgajateljica add foreign key (odgojnaSkupina) references odgojnaSkupina(sifra);
+
+select * from odgojnaSkupina;
+
+insert into odgojnaSkupina (naziv,starost)
+values
+	('bubamara','3'),
+	('tratinèica','4'),
+	('leptiriæi','5');
+
+select * from dijete;
+
+insert into dijete (ime,starost,odgojnaSkupina)
+values
+	('Jure','3',1),
+	('Ivan','4',2),
+	('Valentina','5',3);
+
+select * from odgajateljica;
+
+insert into odgajateljica (ime,prezime,strucnaSprema,odgojnaSkupina)
+values
+	('Marija','Mariæ','odgajateljica',1),
+	('Ljiljana','Lijiæ','odgajateljica',2),
+	('Ðurða','Ðuriæ','odgajateljica',3);
+
+
+select * from dijete;
+
+update dijete set starost='2'
+where sifra=1;
+
+select * from odgajateljica;
+
+delete from odgajateljica where sifra=3;
+
