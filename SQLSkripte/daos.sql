097432f2ac2020f89df9464d0a0fa905872d7d99
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-05-27 15:19:14 +0200

    Create daos.sql

---
+++ SQLSkripte/daos.sql
@@ -0,0 +1,26 @@
+use master;
+drop database if exists daos;
+go
+create database daos;
+go
+use daos;
+
+create table korisnik(
+	sifra int not null primary key identity (1,1),
+	ime varchar (50) not null,
+	prezime varchar (50) not null,
+	telefonskiBroj varchar (10) not null,
+);
+
+create table gradivo(
+	sifra int not null primary key identity (1,1),
+	vrsta varchar (50),
+	status varchar (50),
+	korisnik int not null
+);
+
+create table obrazac(
+	sifra int not null primary key identity (1,1),
+	korisnik int not null,
+	gradivo int not null
+);
