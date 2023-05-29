9bbb8bbf9ba56e05282944fec24d23367bdf1f0d
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-05-29 23:05:35 +0200

    Update daos.sql

--- SQLSkripte/tesko/daos.sql
+++ SQLSkripte/tesko/daos.sql
@@ -1,3 +1,16 @@
+/*Zbirka matiènih knjiga DAOS
+Zbirka matiènih knjiga Državnog arhiva u Osijeku trenutno broji skoro 1400 matiènih knjiga roðenih, 
+vjenèanih i umrlih rimokatolièke, pravoslavne, židovske te drugih vjeroispovijesti iz razdoblja od 1686. 
+do 1900. godine s podruèja Osjeèko-baranjske, Virovitièko-podravske i Vukovarsko-srijemske županije. 
+Pravo na korištenje arhivskog gradiva prema Pravilniku o korištenju arhivskog gradiva imaju svi korisnici
+pod jednakim uvjetima. Meðutim, javno gradivo koje se odnosi na osobne podatke, kao što su matiène knjige, 
+dostupno je za korištenje 70 godina nakon svog nastanka, odnosno 100 godina od roðenja osobe na koju se odnosi. 
+No, kao i sve ostalo arhivsko gradivo koje se èuva u arhivskim ustanovama na korištenje se daje iskljuèivo u 
+arhivskim èitaonicama gdje korisnik podnosi pisani zahtjev na posebnom obrascu. Ovim projektom cilj je 
+korisnicima omoguæiti pregled i rezervaciju arhivskog gradiva te time znatnije vremenski skratiti postupak 
+rješavanja zahtjeva korisnika i dostupnosti gradiva.
+*/
+
 use master;
 drop database if exists daos;
 go
@@ -28,3 +41,4 @@ create table obrazac(
 alter table gradivo add foreign key (korisnik) references korisnik(sifra);
 alter table obrazac add foreign key (korisnik) references korisnik(sifra);
 alter table obrazac add foreign key (gradivo) references gradivo(sifra);
+
