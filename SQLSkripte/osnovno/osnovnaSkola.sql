c52258fb2dbe34444b93d12ef24f55e1e822f274
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-05-31 13:29:42 +0200

    Create osnovnaSkola.sql

---
+++ SQLSkripte/osnovno/osnovnaSkola.sql
@@ -0,0 +1,33 @@
+use master;
+drop database if exists osnovnaSkola;
+go
+create database osnovnaSkola;
+go
+use osnovnaSkola;
+
+create table djecjaRadionica(
+	sifra int not null primary key identity (1,1),
+	naziv varchar (50) not null,
+	brojdjece varchar (50),
+	uciteljica int not null
+);
+
+create table dijete(
+	sifra int not null primary key identity (1,1),
+	ime varchar (50) not null,
+	prezime varchar (50) not null,
+	oib char (11)
+);
+
+create table uciteljica(
+	sifra int not null primary key identity (1,1),
+	ime varchar (50) not null,
+	prezime varchar (50) not null,
+	radionica varchar (50)
+);
+
+create table radionica_dijete(
+	sifra int not null primary key identity (1,1),
+	dijete int,
+	radionica int
+);
