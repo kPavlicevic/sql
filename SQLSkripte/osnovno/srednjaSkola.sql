b6a232cf0c1e2561e363d692eb47bf6d2aa1a281
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-01 23:23:46 +0200

    insert

--- SQLSkripte/osnovno/srednjaSkola.sql
+++ SQLSkripte/osnovno/srednjaSkola.sql
@@ -35,3 +35,37 @@ create table razred_profesor(
 alter table ucenik add foreign key (razred) references razred(sifra);
 alter table razred_profesor add foreign key (razred) references razred(sifra);
 alter table razred_profesor add foreign key (profesor) references profesor(sifra);
+
+
+select * from razred;
+
+insert into razred (naziv,profesor)
+values
+	('1C',1),
+	('2G',2),
+	('4B',3);
+
+select * from ucenik;
+
+insert into ucenik (ime,prezime,oib,razred)
+values
+	('Andrija','Andriæ','21579630158',2),
+	('Karlo','Karliæ','02589631478',3),
+	('Krešo','Krešiæ','03215698745',4);
+
+select * from profesor;
+
+insert into profesor (ime,prezime,razred)
+values
+	('Jasna','Jasniæ',2),
+	('Josip','Joziæ',3),
+	('Nada','Nadiæ',4);
+
+select * from razred_profesor;
+
+insert into razred_profesor (razred,profesor)
+values
+	(2,1),
+	(3,2),
+	(4,3);
+
