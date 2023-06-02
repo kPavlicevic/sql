4353525eecfc63ebbbccb1fbc6ac794965d0e6ad
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-02 18:54:25 +0200

    Create trgovackiCentar.sql

---
+++ SQLSkripte/osnovno/trgovackiCentar.sql
@@ -0,0 +1,26 @@
+use master;
+drop database if exists trgovackiCentar;
+go
+create database trgovackiCentar;
+go
+use trgovackiCentar;
+
+create table trgovina(
+	sifra int not null primary key identity (1,1),
+	naziv varchar (50) not null,
+	sef varchar (50) not null
+);
+
+create table osoba(
+	sifra int not null primary key identity (1,1),
+	ime varchar (50) not null,
+	prezime varchar (50) not null,
+	oib char (11),
+	sef varchar (50) not null
+);
+
+create table trgovina_osoba(
+	sifra int not null primary key identity (1,1),
+	trgovina int not null,
+	osoba int not null
+);
