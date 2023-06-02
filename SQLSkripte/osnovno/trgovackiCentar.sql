6e2617d686e0c3bff6f25e81e72ec2ded5a4f53d
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-02 19:09:34 +0200

    insert

--- SQLSkripte/osnovno/trgovackiCentar.sql
+++ SQLSkripte/osnovno/trgovackiCentar.sql
@@ -27,3 +27,28 @@ create table trgovina_osoba(
 
 alter table trgovina_osoba add foreign key (trgovina) references trgovina(sifra);
 alter table trgovina_osoba add foreign key (osoba) references osoba(sifra);
+
+
+select * from trgovina;
+
+insert into trgovina (naziv,sef)
+values
+	('Adidas','Ivan'),
+	('Pull&Bear','Ivona'),
+	('C&A','Marina');
+
+select * from osoba;
+
+insert into osoba (ime,prezime,oib,sef)
+values
+	('Jasmina','Jasniæ','57963254125','Ivan'),
+	('Ana','Aniæ','59875632015','Ivona'),
+	('Lidija','Lijiæ','98623015485','Marina');
+
+select * from trgovina_osoba;
+
+insert into trgovina_osoba (trgovina,osoba)
+values
+	(1,1),
+	(2,2),
+	(3,3);
