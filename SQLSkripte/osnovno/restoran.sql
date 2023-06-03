61bee680b471345220da23881ee51ded62dab0dd
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-03 22:17:08 +0200

    insert

--- SQLSkripte/osnovno/restoran.sql
+++ SQLSkripte/osnovno/restoran.sql
@@ -38,17 +38,35 @@ alter table jelo_pice add foreign key (jelo) references jelo(sifra);
 alter table jelo_pice add foreign key (pice) references pice(sifra);
 
 
---select * from jelovnik;
+select * from jelovnik;
 
---insert into jelovnik (kategorija)
---values
---	('predjelo'),
---	('glavno jelo'),
---	('desert'),
---	('pice');
+insert into jelovnik (kategorija)
+values
+	('predjelo'),
+	('glavno jelo'),
+	('desert'),
+	('pice');
 
---select * from jelo;
+select * from jelo;
 
---insert into jelo (naziv,jelovnik)
---values
---	('Juha od gljiva',
+insert into jelo (naziv,jelovnik)
+values
+	('Juha od gljiva',1),
+	('Piletina i pommes',2),
+	('Pita od jabuke',3);
+
+select * from pice;
+
+insert into pice (naziv,vrsta)
+values
+	('Karlovaèko','alkohol'),
+	('Kava','topli napitak'),
+	('Juice','sok');
+
+select * from jelo_pice;
+
+insert into jelo_pice (jelo,pice)
+values
+	(2,1),
+	(3,2),
+	(4,3);
