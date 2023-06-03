fabe983693ee8a1a8b91079d9aa498ce06c40635
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-03 21:51:03 +0200

    Create restoran.sql

---
+++ SQLSkripte/osnovno/restoran.sql
@@ -0,0 +1,39 @@
+/*15.	Restoran 
+Restoran ima više kategorija na jelovniku. Jedna kategorija ima više jela. 
+Uz svako jelo može se ponuditi više piæa. Jedno piæe se može ponuditi uz više jela.
+*/
+
+use master;
+drop database if exists restoran;
+go
+create database restoran;
+go
+use restoran;
+
+create table jelovnik (
+	sifra int not null primary key identity (1,1),
+	kategorija varchar (50) not null
+);
+
+create table jelo (
+	sifra int not null primary key identity (1,1),
+	naziv varchar (50) not null,
+	obrok varchar (50),
+	jelovnik int not null
+);
+
+create table pice (
+	sifra int not null primary key identity (1,1),
+	naziv varchar (50) not null,
+	vrsta varchar (50) not null
+);
+
+create table jelo_pice (
+	sifra int not null primary key identity (1,1),
+	jelo int not null,
+	pice int not null
+);
+
+alter table jelo add foreign key (jelovnik) references jelovnik(sifra);
+alter table jelo_pice add foreign key (jelo) references jelo(sifra);
+alter table jelo_pice add foreign key (pice) references pice(sifra);
