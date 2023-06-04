241f5d747dcafa02c286bb7f8067954d1bb98f85
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-04 13:26:03 +0200

    insert,update,delete

--- SQLSkripte/osnovno/vodoinstalater.sql
+++ SQLSkripte/osnovno/vodoinstalater.sql
@@ -38,3 +38,42 @@ create table popravak_kvar(
 alter table popravak_kvar add foreign key (popravak) references popravak(sifra);
 alter table popravak_kvar add foreign key (kvar) references kvar(sifra);
 alter table popravak_kvar add foreign key (segrt) references segrt(sifra);
+
+
+select * from popravak;
+
+insert into popravak (cijena)
+values
+	('25.60'),
+	('104.34'),
+	('238.45');
+
+select * from kvar;
+
+insert into kvar (vrsta)
+values
+	('wc školjka'),
+	('sudoper'),
+	('umivaonik');
+
+select * from segrt;
+
+insert into segrt (ime,prezime)
+values
+	('Ivan','Iviæ'),
+	('Zoran','Zoriæ'),
+	('Vale','Valiæ');
+
+select * from popravak_kvar;
+
+insert into popravak_kvar (popravak,kvar,segrt)
+values
+	(2,1,1),
+	(3,2,2),
+	(4,3,3);
+
+update kvar set vrsta='cijev'
+where sifra=1;
+
+delete from popravak_kvar where popravak=2;
+delete from popravak where sifra=2;
